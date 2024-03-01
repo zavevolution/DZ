f1183fef16ce9d661b814c7a988f767afc3e7b05
Zahrafov <evolutionzav@gmail.com>
2024-03-01 21:29:58 +0100

    My home work oop style.

--- src/oop/Shop.java
+++ src/oop/Shop.java
@@ -2,5 +2,27 @@ package oop;
 
 public class Shop {
     private double price;
+    private double[] result;
+    private double initialWeight;
 
+    public Shop(double price) {
+        this.price = price;
+        result = new double[5];
+        initialWeight = 1.2;
+    }
+
+    private void getPrices() {
+        int index = 0;
+        for (double i = 1.2; i <= 2; i += 0.2) {
+            result[index] = price * i;
+            index++;
+        }
+    }
+
+    public void show() {
+        for (double val : result) {
+            System.out.printf("%.1fкг. цукерок, коштує %.2fгрн.\n", initialWeight, val);
+            initialWeight += 0.2;
+        }
+    }
 }
